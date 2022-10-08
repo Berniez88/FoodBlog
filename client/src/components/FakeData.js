5405a2de94008b9e2c8e7fbb639ebd0ce5afb635
bzhong88 <58304673+Berniez88@users.noreply.github.com>
2022-10-08 17:22:35 -0400

    fetching the dummy data code

--- client/src/components/FakeData.js
+++ client/src/components/FakeData.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function FakeData() {
+  const [fakeData, setFakeData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/data")
+      .then((res) => res.json())
+      .then((data) => setFakeData(data));
+  }, []);
   return (
     <>
-      <h1>data</h1>
+      <h1>Fake Data</h1>
+      <ul>
+        {fakeData.map((individualData) => {
+          return (
+            <li key={individualData.id}>
+              {individualData.id} {individualData.name}
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
